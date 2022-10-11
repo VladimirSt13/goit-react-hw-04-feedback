3bb1451b1a5dceef66704d7a080a02da55955d0e
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-10-11 22:46:15 +0300

    refactor to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,67 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Container } from './App.styled';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const state = { good, neutral, bad };
 
-  handlClickFeedback = key => {
-    this.setState(prevState => {
-      return {
-        [key]: prevState[key] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = feedBacks =>
+  const countTotalFeedback = feedBacks =>
     feedBacks.reduce((sum, value) => (sum += value), 0);
 
-  countPositiveFeedbackPercentage = feedBacks => {
+  const countPositiveFeedbackPercentage = feedBacks => {
     const positiveFeedBackPercentage =
-      (feedBacks[0] / this.countTotalFeedback(feedBacks)) * 100;
+      (feedBacks[0] / countTotalFeedback(feedBacks)) * 100;
     return isNaN(positiveFeedBackPercentage)
       ? '0'
       : Math.round(positiveFeedBackPercentage * 10) / 10;
   };
 
-  render() {
-    const feedbackValues = Object.values(this.state);
-    const total = this.countTotalFeedback(feedbackValues);
-    const positivePercentage =
-      this.countPositiveFeedbackPercentage(feedbackValues);
+  const handlClickFeedback = key => {
+    switch (key) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        break;
+    }
+  };
 
-    return (
-      <Container>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handlClickFeedback}
+  const feedbackValues = Object.values(state);
+  const total = countTotalFeedback(feedbackValues);
+  const positivePercentage = countPositiveFeedbackPercentage(feedbackValues);
+
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(state)}
+          onLeaveFeedback={handlClickFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {!total ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            stats={state}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {!total ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              stats={this.state}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </Section>
-      </Container>
-    );
-  }
-}
+        )}
+      </Section>
+    </Container>
+  );
+};
